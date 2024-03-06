0e9574aede87500eb1e386d678d9d69809d9edc6
ShivamMishra828 <shivam9878522087@gmail.com>
2024-03-06 20:17:26 +0530

    Added React-Hook-Form and Yup Validation and created Login Page using that

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -7,6 +7,7 @@ import DashboardLayout from "../layouts/dashboard";
 // config
 import { DEFAULT_PATH } from "../config";
 import LoadingScreen from "../components/LoadingScreen";
+import MainLayout from "../layouts/main";
 
 const Loadable = (Component) => (props) => {
   return (
@@ -18,6 +19,15 @@ const Loadable = (Component) => (props) => {
 
 export default function Router() {
   return useRoutes([
+    {
+      path: "/auth",
+      element: <MainLayout />,
+      children: [
+        { path: "login", element: <Login /> },
+        { path: "404", element: <Page404 /> },
+        { path: "*", element: <Navigate to="/404" replace /> },
+      ],
+    },
     {
       path: "/",
       element: <DashboardLayout />,
@@ -38,5 +48,7 @@ const GeneralApp = Loadable(
 );
 
 const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
+
+const Login = Loadable(lazy(() => import("../pages/auth/Login")));
 
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
---
+++ client/src/sections/auth/AuthSocial.js
@@ -0,0 +1,35 @@
+import { Box, Divider, IconButton, Stack } from "@mui/material";
+import { GithubLogo, GoogleLogo, TwitterLogo } from "phosphor-react";
+import React from "react";
+
+const AuthSocial = () => {
+  return (
+    <Box>
+      <Divider
+        sx={{
+          my: 2.5,
+          typography: "overline",
+          color: "text.disabled",
+          "&::before, &::after": {
+            borderTopStyle: "dashed",
+          },
+        }}
+      >
+        Or continue with
+      </Divider>
+      <Stack direction={"row"} justifyContent={"center"} spacing={2}>
+        <IconButton>
+          <GoogleLogo color="#df3e30" />
+        </IconButton>
+        <IconButton color="inherit">
+          <GithubLogo />
+        </IconButton>
+        <IconButton>
+          <TwitterLogo color="#1c9cea" />
+        </IconButton>
+      </Stack>
+    </Box>
+  );
+};
+
+export default AuthSocial;
