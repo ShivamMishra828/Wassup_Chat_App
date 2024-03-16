48d51fbb8d6754190ad6bfbbd898f70c92ecc3d4
ShivamMishra828 <shivam9878522087@gmail.com>
2024-03-16 11:16:47 +0530

    Update socket URL in connectSocket function

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,8 +2,11 @@ import io from "socket.io-client";
 
 let socket;
 
+const url =
+  "http://localhost:5000" || "https://wassup-chat-app-y71b.onrender.com";
+
 const connectSocket = (user_id) => {
-  socket = io("http://localhost:5000", {
+  socket = io(url, {
     query: `user_id=${user_id}`,
   });
 };
